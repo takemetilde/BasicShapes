9c372d901c666020d3288652e110365076a37b73
hongiaher <hongiaher@gmail.com>
2016-09-15 10:02:08 -0400

    Update Rectangle2D.java

--- Rectangle2D.java
+++ Rectangle2D.java
@@ -4,6 +4,18 @@ public class Rectangle2D extends Shape{
 	private MyPoint p1, p2, p3, p4;
 	private double area;
 	private double perimeter;
+
+        Rectangle2D(){
+        }
+
+        Rectangle2D(MyPoint p1, MyPoint p2, MyPoint p3, MyPoint p4){
+        This.p1 = p1;
+        This.p2 = p2;
+        This.p3 = p3;
+        This.p4 = p4;
+        }
+
+        Public double 
 	
 	
 }
